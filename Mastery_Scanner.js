431cd2cd67c4589f71ee3b90490a2899516e5b8c
Bl00D4NGEL <kuhlesdominik@gmx.de>
2016-07-31 02:39:48 +0200

    Update Mastery_Scanner.js

--- Mastery_Scanner.js
+++ Mastery_Scanner.js
@@ -1,27 +1,37 @@
 // ==UserScript==
 // @name         Mastery-Scanner
-// @version      1.0
+// @version      1.1
 // @description  Click on all items in the mastery page and finally print out the result in the console
 // @author       Dominik "Bl00D4NGEL" Peters
-// @match        http://www.drakor.com/masteries
+// @match        http://*.drakor.com/masteries
 // ==/UserScript==
 
-var tradeskills = [document.getElementsByClassName("tradeMat Logging"),
+var tradeskills = [document.getElementsByClassName("tradeMat TreasureHunting"),
                    document.getElementsByClassName("tradeMat Mining"),
                    document.getElementsByClassName("tradeMat Fishing"),
                    document.getElementsByClassName("tradeMat Gathering"),
                    document.getElementsByClassName("tradeMat AncientResearch"),
-                   document.getElementsByClassName("tradeMat Disenchanting")];
-var displayArray = ["Logging","Mining","Fishing","Gathering","Ancient Research", "Disenchanting"];
-var masteryBar = document.getElementsByClassName("masteryBar");
+                   document.getElementsByClassName("tradeMat Disenchanting"),
+                   document.getElementsByClassName("tradeMat Logging")];
+var tradeIds = [[],[],[],[],[],[],[]]; //2d array for ids of each tradeskill
+var tradeIdIndex = 0;
+var displayArray = ["Treasure Hunting","Mining","Fishing","Gathering","Ancient Research", "Disenchanting", "Logging"];
 var speedMulti = 100; //The lower this amount the faster it goes (200-500 is alright)
-function ClickAllItems(tradeskill, i, sleepTime){
+var output = "";
+var totalAttempts = "";
+var totalTradeItems = 0;
+
+function ClickAllItems(tradeskill, sleepTime){
+    var i=0;
     setTimeout(function(){
-        myInterval = setInterval(function(){
-            if(i<tradeskill.length){
+        var myInterval = setInterval(function(){
+            if(tradeskill.length > i && tradeskill[i].id !== ""){
+                var id = tradeskill[i].id.slice(4);
+                tradeIds[tradeIdIndex].push(id);
                 tradeskill[i].click();
             }
             else{
+                tradeIdIndex++;
                 clearInterval(myInterval);
             }
             i++;
@@ -29,35 +39,35 @@ function ClickAllItems(tradeskill, i, sleepTime){
     }, sleepTime);
 
 }
-var output = "";
-var totalAttempts = "";
 
-function PrintMasteryScore(tradeskill, displayTrade, index, dummy){
-        var totalMastery = 0;
-        for(var j=0;j<(tradeskill.length-1);j++){
-            if(tradeskill[j].innerText.valueOf("x") !== -1){
-                output += "\n" + tradeskill[j].innerText.slice(0,-1)+ " - Mastery score: " + masteryBar[(index+j-dummy)].innerText;
-                totalMastery += Number(masteryBar[(index+j-dummy)].innerText.slice(1,masteryBar[(index+j-dummy)].innerText.indexOf("/")).replace(",",""));
-            }
-        }
-        totalAttempts +=  "\n" +"Total attempts done in " + displayTrade + ": " + totalMastery;
-}
-/*I know this is ugly as heck, but I currently don't see any other way to avoid clicking items in the wrong order in any other way */
-ClickAllItems(tradeskills[0],0,speedMulti);
-ClickAllItems(tradeskills[1],0, tradeskills[0].length * speedMulti + speedMulti * 10);
-ClickAllItems(tradeskills[2],0, (tradeskills[0].length + tradeskills[1].length) * speedMulti+speedMulti*20);
-ClickAllItems(tradeskills[3],0, (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length) * speedMulti+speedMulti*30);
-ClickAllItems(tradeskills[4],0, (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length + tradeskills[3].length) * speedMulti+speedMulti*40);
-ClickAllItems(tradeskills[5],0, (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length + tradeskills[3].length + tradeskills[4].length) * speedMulti+speedMulti*50);
+/*I know this is ugly as heck, but I currently don't see any other way to avoid clicking items in the wrong order in any other way*/
+ClickAllItems(tradeskills[0],speedMulti); //100
+ClickAllItems(tradeskills[1], (tradeskills[0].length +10) * speedMulti);
+ClickAllItems(tradeskills[2], (tradeskills[0].length + tradeskills[1].length + 20) * speedMulti);
+ClickAllItems(tradeskills[3], (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length + 30) * speedMulti);
+ClickAllItems(tradeskills[4], (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length + tradeskills[3].length + 40) * speedMulti);
+ClickAllItems(tradeskills[5], (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length + tradeskills[3].length + tradeskills[4].length + 50) * speedMulti);
+ClickAllItems(tradeskills[6], (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length + tradeskills[3].length + tradeskills[4].length + tradeskills[5].length + 60) * speedMulti);
+var totalTradeItems = tradeskills[0].length + tradeskills[1].length + tradeskills[2].length + tradeskills[3].length + tradeskills[4].length + tradeskills[5].length + tradeskills[6].length;
 
 
-/*I know this is ugly as heck, but I currently don't see any other way to avoid displaying the wrong masteries for the wrong skill in any other way */
 setTimeout(function(){
-PrintMasteryScore(tradeskills[0], displayArray[0],0,0);
-PrintMasteryScore(tradeskills[1], displayArray[1], tradeskills[0].length,1);
-PrintMasteryScore(tradeskills[2], displayArray[2], (tradeskills[0].length + tradeskills[1].length),2);
-PrintMasteryScore(tradeskills[3], displayArray[3], (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length),3);
-PrintMasteryScore(tradeskills[4], displayArray[4], (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length + tradeskills[3].length),4);
-PrintMasteryScore(tradeskills[5], displayArray[5], (tradeskills[0].length + tradeskills[1].length + tradeskills[2].length + tradeskills[3].length + tradeskills[4].length),5);
-console.log(output, totalAttempts);
-}, speedMulti * 250);
+    for(var i=0;i<tradeIds.length;i++){
+        var totalMastery = 0;
+        output += "\n\n\n***TRADE:" + displayArray[i].toUpperCase() + "***\n\n";
+        for(var j=0;j<tradeIds[i].length;j++){
+            var materialId = "mat-" + tradeIds[i][j];
+            var material = document.getElementById(materialId).innerText;
+            var masteryId = "mat" + tradeIds[i][j];
+            var mastery = document.getElementById(masteryId).getElementsByClassName("matPopupContainer")[0].getElementsByClassName("bv_success")[0].getElementsByClassName("masteryBox")[0].getElementsByClassName("masteryBar")[0].innerText;
+            var attempts = Number(mastery.slice(0,mastery.indexOf("/")-1).replace(",",""));
+            totalMastery += attempts;
+            output += "\n" + material.slice(1, material.indexOf("]")) + "\t - \t" +  mastery.slice(0, mastery.indexOf("("));
+        }
+        totalAttempts += "\n\nTrade:\t" + displayArray[i] + "\nTotal attempts:\t" + totalMastery + "\n";
+    }
+    console.log(output, totalAttempts);
+}, (totalTradeItems  + 70) * speedMulti);
+
+
+
