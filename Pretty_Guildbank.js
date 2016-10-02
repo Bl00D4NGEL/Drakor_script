a0ce9477d646c0db560f34277146e81a4a668e2d
Bl00D4NGEL <kuhlesdominik@gmx.de>
2016-10-02 16:22:59 +0200

    Shortened the code
    Same functionality, less code and more dynamic.

--- Pretty_Guildbank.js
+++ Pretty_Guildbank.js
@@ -1,223 +1,69 @@
 // ==UserScript==
 // @name         Pretty guild bank
-// @version      1.0
+// @version      1.1
 // @description  Sort the guild bank a little prettier
 // @author       Dominik "Bl00D4NGEL" Peters
 // @match        http://*.drakor.com/guild*
+// @match        https://*.drakor.com/guild*
 // ==/UserScript==
-function Main() {
-    var items = document.getElementsByClassName("world_bank");
-    var gblength = document.getElementsByClassName("guildContent")[0].childNodes[3].childNodes.length;
-    var guildBankContent = document.getElementsByClassName("guildContent")[0].childNodes[3];
-    if (document.getElementById("displayResults") !== null) {
-        gblength = document.getElementById("displayResults").childNodes[1].childNodes.length;
-        guildBankContent = document.getElementById("displayResults").childNodes[1];
-    }
-    var fragment = document.createElement("fragment");
-    var headingRing = document.createElement("span");
-    headingRing.innerHTML = "<h2>Rings</h2>";
-    var headingTool = document.createElement("span");
-    headingTool.innerHTML = "<h2>Tools</h2>";
-    var headingEnchant = document.createElement("span");
-    headingEnchant.innerHTML = "<h2>Enchants</h2>";
-    var headingAugment = document.createElement("span");
-    headingAugment.innerHTML = "<h2>Augments</h2>";
-    var headingDura = document.createElement("span");
-    headingDura.innerHTML = "<h2>Durabity Scrolls</h2>";
-    var headingFood = document.createElement("span");
-    headingFood.innerHTML = "<h2>Food</h2>";
-    var headingItem = document.createElement("span");
-    headingItem.innerHTML = "<h2>Items</h2>";
-    var headingTeleport = document.createElement("span");
-    headingTeleport.innerHTML = "<h2>Teleports</h2>";
-    var headingBattle = document.createElement("span");
-    headingBattle.innerHTML = "<h2>Spells</h2>";
-    console.log("gblength: " + gblength);
-    for (var i = gblength - 1; i > -1; i--) {
-        var imageText = document.getElementsByClassName("world_bank")[i].childNodes[3].childNodes[1].src;
-        console.log("imageText: " + imageText + "\t i: " + i);
-        var regRing = /\/ring/gi;
-        var regTool = /tool/gi;
-        var regEnchant = /enchant/gi;
-        var regAugment = /\/images\/enhancements\/aug\d+\.png/;
-        var regTeleport = /teleport/gi;
-        var regDura = /dura/gi;
-        var regFood = /food/gi;
-        var regItem = /\/items/gi;
-        var regBattle = /battle\/.*/gi;
-        var match = imageText.match(regRing);
-        if (imageText.match(regRing) !== null) {
-            headingRing.appendChild(items[i]);
-        }
-        else if (imageText.match(regTool) !== null) {
-            headingTool.appendChild(items[i]);
-        }
-        else if (imageText.match(regItem) !== null) {
-            headingItem.appendChild(items[i]);
-        }
-        else if (imageText.match(regEnchant) !== null) {
-            headingEnchant.appendChild(items[i]);
-        }
-        else if (imageText.match(regAugment) !== null) {
-            headingAugment.appendChild(items[i]);
-        }
-        else if (imageText.match(regTeleport) !== null) {
-            headingTeleport.appendChild(items[i]);
-        }
-        else if (imageText.match(regDura) !== null) {
-            headingDura.appendChild(items[i]);
-        }
-        else if (imageText.match(regFood) !== null) {
-            headingFood.appendChild(items[i]);
-        }
-        else {
-            headingBattle.appendChild(items[i]);
-        }
-    }
-    // headingRing = Sort_By_Tradeskill(headingRing, "<h2>Rings</h2>");
-    // headingTool = Sort_By_Tradeskill(headingTool, "<h2>Tools</h2>");
-    // headingFood = Sort_By_Food_Type(headingFood);
-    fragment.appendChild(headingRing);
-    fragment.appendChild(headingTool);
-    fragment.appendChild(headingEnchant);
-    fragment.appendChild(headingAugment);
-    fragment.appendChild(headingItem);
-    fragment.appendChild(headingDura);
-    fragment.appendChild(headingFood);
-    fragment.appendChild(headingTeleport);
-    fragment.appendChild(headingBattle);
-    guildBankContent.appendChild(fragment);
-}
-function Sort_By_Tradeskill(spanObjects, title) {
-    var tradeskillsDict = {};
-    var tradeskills = ["Fis", //Fishing
-                       "Min", //Mining
-                       "Gat", //Gathering
-                       "Log", //Logging
-                       "Res", //Ancient Research
-                       "Dis", //Disenchanting
-                       "Enc", //Enchanting
-                       "Smi", //Smithing
-                       "Jew", //Jewelcrafting
-                       "Con", //Construction
-                       "Alc", //Alchemy
-                       "Ins", //Inscription
-                       "Cra", //Crafting
-                       "Tre", //Treasure Hunting
-                       "Coo"];//Cooking
-    var newSpan = document.createElement("span");
-    newSpan.innerHTML = title; //Because we return a new "span" element the old title is gone
-    for (var i = 0; i < tradeskills.length; i++) {
-        tradeskillsDict[tradeskills[i]] = [];
-        //First cut out all the elements and put them in the corresponding array
-        for (var j = spanObjects.childNodes.length - 1; j > 0; j--) {
-            if (spanObjects.childNodes[j].childNodes[1].innerText.indexOf(tradeskills[i]) !== -1) {
-                tradeskillsDict[tradeskills[i]].push(spanObjects.removeChild(spanObjects.childNodes[j]));
+function Main(){
+    var items = $(".drIcon");
+    var headingSize = "h2";
+    var headings = {
+        "Rings": /\/ring/i,
+        "Tools": /tool/i,
+        "Food": /food/i,
+        "Teleports": /teleport/i,
+        "Spells": /battle\/[^tp]/i,
+        "Equipment": /\/items\/[^ringtol]/i,
+        "Enchants": /enhancements\/enchant/i,
+        "Augments":  /\/images\/enhancements\/aug\d+\.png/,
+        "Durability Scrolls/ Runes": /(dura|rune)/i
+    };
+    for(var heading in headings){
+        var span = $(document.createElement(headingSize)).attr({id:"heading-" + heading.replace(" ", "_")}).html(heading + "<br/>").css("text-align", "center").insertBefore($(".tradeInventory"));
+        for(var i=0;i<items.length;i++){
+            if($(items[i]).find(".centerImage").attr('src').match(headings[heading])){
+                $(items[i]).appendTo(span);
             }
         }
-        //After that iterate over array entries and put them back into the span
-        for (var k = 0; k < tradeskillsDict[tradeskills[i]].length; k++) {
-            newSpan.appendChild(tradeskillsDict[tradeskills[i]].pop());
-        }
     }
-    return newSpan;
 }
-function Sort_By_Food_Type(spanObjects) {
-    var foodDict = {};
-    var foodTypes = [
-        "Drop",
-        "Create",
-        "Invis",
-        "Gold",
-        "Travel",
-        "Treasure",
-    ];
-    var newSpan = document.createElement("span");
-    newSpan.innerHTML = "<h2>Food</h2>"; //Because we return a new "span" element the old title is gone
-    for (var i = 0; i < foodTypes.length; i++) {
-        foodDict[foodTypes[i]] = [];
-        //First cut out all the elements and put them in the corresponding array
-        for (var j = spanObjects.childNodes.length - 1; j > 0; j--) {
-            if (spanObjects.childNodes[j].childNodes[1].innerText.indexOf(foodTypes[i]) !== -1) {
-                foodDict[foodTypes[i]].push(spanObjects.removeChild(spanObjects.childNodes[j]));
-            }
-        }
-        //After that iterate over array entries and put them back into the span
-        for (var k = 0; k < foodDict[foodTypes[i]].length; k++) {
-            newSpan.appendChild(foodDict[foodTypes[i]].pop());
-        }
-    }
-    return newSpan;
-}
-function post(path, params, method) {
-    method = method || "post"; // Set method to post by default if not specified.
 
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-
-    for (var key in params) {
-        if (params.hasOwnProperty(key)) {
-            var hiddenField = document.createElement("input");
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("name", key);
-            hiddenField.setAttribute("value", params[key]);
-
-            form.appendChild(hiddenField);
-        }
-    }
-    document.body.appendChild(form);
-    form.submit();
-}
-function WithdrawXAmount(removeItemElement) {
-
-}
-$(document).ready(function () {
+$(document).ready(function(){
     Main();
-    $("body").on("dblclick", ".tradeMat , .world_bank", function (event) {
+    $("body").on("dblclick", ".tradeMat , .world_bank", function(event){
         console.log(event);
         var id;
-        if (event.currentTarget.id) {
+        if(event.currentTarget.id){
             id = event.currentTarget.id;
         }
-        var postfix = "-1";
-        var prefix = "item-";
-        if (id.indexOf("mat") !== -1) {
-            postfix = "-2";
-            prefix = "mat-";
-            if (event.ctrlKey) {
-                var curAmount = event.currentTarget.childNodes[1].nodeValue.replace(/[^\d]/g, "");
-                var amountInput = document.createElement("input");
-                amountInput.type = "Number";
-                amountInput.value = curAmount;
-                amountInput.style.width = "125px";
-                amountInput.style.fontSize = "12px";
-                amountInput.id = "input" + id;
-                amountInput.max = curAmount;
-                event.currentTarget.removeChild(event.currentTarget.childNodes[1]);
-                event.currentTarget.insertBefore(amountInput, event.currentTarget.childNodes[0]);
-                $(event.currentTarget).on("keydown", function (event) {
-                    if (event.keyCode === 13) {
-                        console.log(event.currentTarget.childNodes[0].value);
-                        params = {
-                            removeType: "",
-                            removeItem: id + postfix,
-                            itemQty: event.currentTarget.childNodes[0].value
-                        };
-                        post("/guild/bank/withdrawal", params);
+        else{
+            alert("Double Click withdrawing is not supported in this browser");
+            return;
+        }
+        var data = "item-$-1";
+        if(id.indexOf("mat") !== -1){ //Tradeskill item
+            console.log($(event.currentTarget).text());
+            data = "mat-$-2";
+            if(event.ctrlKey){
+                var curAmount = $(event.currentTarget).text().match(/(\d+)/i)[1];
+                var amountInput = $(document.createElement("input")).attr({type: "Number", value: curAmount, max: curAmount, id: "input"+ id})
+                .css({width: "125px", "font-size": "12px"}).insertBefore($(event.currentTarget).find("span"));
+                $(event.currentTarget).on("keydown",function(event){
+                    if(event.keyCode === 13){
+                        var form = $(document.createElement("form")).attr({action: "/guild/bank/withdrawal"}).appendTo($("body"));
+                        $(document.createElement("input")).attr({name: "removeItem", value: data.replace("$", id.slice(4))}).appendTo(form);
+                        $(document.createElement("input")).attr({name: "itemQty", value: $("#input" + id).val()}).appendTo(form);
+                        $(form).submit();
                     }
                 });
                 return;
             }
         }
         id = id.slice(4);
-        var params = {
-            removeType: "",
-            removeItem: prefix + id + postfix,
-            itemQty: ""
-        };
-        post("/guild/bank/withdrawal", params);
+        var form = $(document.createElement("form")).attr({action: "/guild/bank/withdrawal", method: "POST"}).appendTo($("body"));
+        $(document.createElement("input")).attr({name: "removeItem", value: data.replace("$", id)}).appendTo(form);
+        $(form).submit();
     });
 });
