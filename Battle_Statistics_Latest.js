ea6c63f2176c497f7c2562ad814c23054c40e0a2
Bl00D4NGEL <kuhlesdominik@gmx.de>
2016-12-31 15:26:42 +0100

    Fixed bugs in both scripts
    Also changed the layout of the Disenchanting for Battle Statistics so that you can now specify each rarity to be DE'd instead of just anything under the selected value

--- Battle_Statistics_Latest.js
+++ Battle_Statistics_Latest.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         Battle-statistics v1.5
-// @version      1.5
+// @name         Battle-statistics v1.51
+// @version      1.51
 // @description  Tracks statistics of battles (Arena and Node)
 // @author       Dominik "Bl00D4NGEL" Peters
 // @match        http://*.drakor.com*
 // @match        https://*.drakor.com*
 // ==/UserScript==
 
+var debug = 0;
 $(document).ready(function () {
 	var version = "v1.5";
-	var debug = 0;
 	SetupLiveLog(); //Live-log-div setup under chat
 	LiveLog("You're currently using Battle Statistics version " + version);
 	CheckInventory(); //To load the durability data into the livelog
@@ -26,9 +26,18 @@ $(document).ready(function () {
 	$(".menuFighting").on("click", function(){
 		CheckInventory();
 	});
-	if(log.CurrentLoot > 0){
+	if(debug > 0){
+		LiveLog("D: Current Loot (Drakor) is " + $("#load-openloot").html().match(/(\d+)/)[1] + " and log.CurrentLoot is " + log.CurrentLoot);
+	}
+	if(log.CurrentLoot > 0 && $("#load-openloot").html().match(/(\d+)/)[1] !== "0"){
+		LiveLog("Set 'Actual Loot' to " + log.CurrentLoot);
 		var lootbagText = $("#load-openloot").html().match(/(.*\d+).*/)[1];
 		$("#load-openloot").html(lootbagText + "(" + log.CurrentLoot + ")");
+	}
+	else if ($("#load-openloot").html().match(/(\d+)/)[1] === "0"){
+		log.CurrentLoot = 0;
+		localStorage.setItem("battleLog", JSON.stringify(log));
+		LiveLog("Setting 'Actual Loot' to 0 because the loot bag got opened without notice");
 	}
 	$( document ).ajaxComplete(function( event, xhr, settings ) {
 		if (settings.url === "/arena") {
@@ -43,75 +52,80 @@ $(document).ready(function () {
 			});
 		}
 		else if(settings.url.match(/\/battle-round\/attack.*/)){
-			var difficulty = $(".battleDiff").html().match(/:\s(\w+).*/)[1];
-			log = JSON.parse(localStorage.getItem("battleLog"));
+			try{
+				var difficulty = $(".battleDiff").html().match(/:\s(\w+).*/)[1];
+				log = JSON.parse(localStorage.getItem("battleLog"));
 
-			if(xhr.responseText.match(/victory/)){
-				// difficulty = xhr.responseText.match(/<h4>(\w+).*?<\/h4>/i)[1];
-				console.log(difficulty);
-				log.Won++;
-				log[difficulty].Won++;
-				var loot = xhr.responseText.match(/modLoot\((\d)\)/);
-				if(loot){ //Got loot
-					var realLoot = xhr.responseText.match(/with\s(\d+)\sitem/i);
-					LiveLog("Victory (" + $($(".roundResult").get(0)).html().match(/(#\d+)/)[1] + ") - Looted " + realLoot[1] + " item" + (Number(realLoot[1]) > 1 ? 's': '') + "!");
-					log.TotalLoot += Number(realLoot[1]);
-					log.CurrentLoot += Number(realLoot[1]);
-					log[difficulty].Loot += Number(realLoot[1]);
-					var lootbagText = $("#load-openloot").html().match(/(.*\d+).*/)[1];
-					var itemsInLootbag = lootbagText.match(/(\d+)/);
-					if(itemsInLootbag[1] === "1"){
-						//LiveLog("Loot bag has been opened");
-						log.CurrentLoot = Number(realLoot[1]);
+				if(xhr.responseText.match(/victory/)){
+					// difficulty = xhr.responseText.match(/<h4>(\w+).*?<\/h4>/i)[1];
+					console.log(difficulty);
+					log.Won++;
+					log[difficulty].Won++;
+					var loot = xhr.responseText.match(/modLoot\((\d)\)/);
+					if(loot){ //Got loot
+						var realLoot = xhr.responseText.match(/with\s(\d+)\sitem/i);
+						LiveLog("Victory (" + $($(".roundResult").get(0)).html().match(/(#\d+)/)[1] + ") - Looted " + realLoot[1] + " item" + (Number(realLoot[1]) > 1 ? 's': '') + "!");
+						log.TotalLoot += Number(realLoot[1]);
+						log.CurrentLoot += Number(realLoot[1]);
+						log[difficulty].Loot += Number(realLoot[1]);
+						var lootbagText = $("#load-openloot").html().match(/(.*\d+).*/)[1];
+						var itemsInLootbag = lootbagText.match(/(\d+)/);
+						if(itemsInLootbag[1] === "1"){
+							//LiveLog("Loot bag has been opened");
+							log.CurrentLoot = Number(realLoot[1]);
+						}
+						$("#load-openloot").html(lootbagText + "(" + log.CurrentLoot + ")");
 					}
-					$("#load-openloot").html(lootbagText + "(" + log.CurrentLoot + ")");
-				}
-				else{
-					LiveLog("Victory (" + $($(".roundResult").get(0)).html().match(/(#\d+)/)[1] + ") - Looted 0 items!");
-					log.WonWithoutLoot++;
-					log[difficulty].WonWithoutLoot++;
-				}
-				var exp = xhr.responseText.match(/([0-9,]+)\s?\w*\s?exp/i);
-				log.Experience += Number(exp[1].replace(",", ""));
-				log[difficulty].Experience += Number(exp[1].replace(",", ""));
-				var gold = xhr.responseText.match(/>([0-9,]+)\sgold/i);
-				if(gold){
-					log.Gold += Number(gold[1].replace(",",""));
-					log[difficulty].Gold += Number(gold[1].replace(",",""));
-				}
-				if($(".menuFighting").length){
-					AddEnterShortcut($(".menuFighting"));
-					$(".menuFighting").on("click", function(){
-						CheckInventory();
-					});
-				}
-				else{
-					AddEnterShortcut($("#load-arena"));
-				}
-			}
-			else if(xhr.responseText.match(/defeated/) && !xhr.responseText.match(/victory/)){
-				LiveLog("Defeat (" + $($(".roundResult").get(0)).html().match(/(#\d+)/)[1] + ")");
-				log.Lost++;
-				log[difficulty].Lost++;
-
-				if(xhr.responseText.match(/modloot/i)){
-					log.ConsolationLoot++;
-					log[difficulty].ConsolationLoot++;
-					log.CurrentLoot++;
-					var bagText = $("#load-openloot").html().match(/(.*\d+).*/)[1];
-					var itemsInBag = bagText.match(/(\d+)/);
-					if(itemsInBag[1] === "1"){
-						//LiveLog("Loot bag has been opened");
-						log.CurrentLoot = 1;
+					else{
+						LiveLog("Victory (" + $($(".roundResult").get(0)).html().match(/(#\d+)/)[1] + ") - Looted 0 items!");
+						log.WonWithoutLoot++;
+						log[difficulty].WonWithoutLoot++;
 					}
-					$("#load-openloot").html(bagText + "(" + log.CurrentLoot + ")");
-
+					var exp = xhr.responseText.match(/([0-9,]+)\s?\w*\s?exp/i);
+					log.Experience += Number(exp[1].replace(",", ""));
+					log[difficulty].Experience += Number(exp[1].replace(",", ""));
+					var gold = xhr.responseText.match(/>([0-9,]+)\sgold/i);
+					if(gold){
+						log.Gold += Number(gold[1].replace(",",""));
+						log[difficulty].Gold += Number(gold[1].replace(",",""));
+					}
+					if($(".menuFighting").length){
+						AddEnterShortcut($(".menuFighting"));
+						$(".menuFighting").on("click", function(){
+							CheckInventory();
+						});
+					}
+					else{
+						AddEnterShortcut($("#load-arena"));
+					}
 				}
+				else if(xhr.responseText.match(/defeated/) && !xhr.responseText.match(/victory/)){
+					LiveLog("Defeat (" + $($(".roundResult").get(0)).html().match(/(#\d+)/)[1] + ")");
+					log.Lost++;
+					log[difficulty].Lost++;
 
-				AddEnterToSpecificClass("navButton", "Back to Adventure");
-				AddEnterToSpecificClass("navButton", "Back to Arena");
+					if(xhr.responseText.match(/modloot/i)){
+						log.ConsolationLoot++;
+						log[difficulty].ConsolationLoot++;
+						log.CurrentLoot++;
+						var bagText = $("#load-openloot").html().match(/(.*\d+).*/)[1];
+						var itemsInBag = bagText.match(/(\d+)/);
+						if(itemsInBag[1] === "1"){
+							//LiveLog("Loot bag has been opened");
+							log.CurrentLoot = 1;
+						}
+						$("#load-openloot").html(bagText + "(" + log.CurrentLoot + ")");
+
+					}
+
+					AddEnterToSpecificClass("navButton", "Back to Adventure");
+					AddEnterToSpecificClass("navButton", "Back to Arena");
+				}
+				localStorage.setItem("battleLog", JSON.stringify(log));
 			}
-			localStorage.setItem("battleLog", JSON.stringify(log));
+			catch(e){
+				LiveLog("Oh that's not good..<br/>" + e.message);
+			}
 		}
 		else if(settings.url.match(/\/battle-create\/.*/)){
 			AddEnterToSpecificClass("navButton", "Start Battle!");
@@ -162,6 +176,8 @@ $(document).ready(function () {
 				}
 			});
 		}
+
+
 		else if(settings.url.match(/\/world\/disenchanting/i)){DisenchantSetup();} //Setup of the selects
 		else if(settings.url.match(/\/world\/action_disenchanting/i)){LiveLog("You disenchanted an item");SelectItemToDisenchant();} //Auto de on enter press
 		else if(settings.url.match(/(chat|sell|show|openloot|battle)/i)){}
@@ -253,10 +269,16 @@ function CheckInventory(){
 		}
 		var inventorySpaces = Number(data.match(/<div\sid=\"char_inventory".*?><div.*?b>([0-9,]+).*?<\/b>/)[1].replace(",",""));
 		var log = JSON.parse(localStorage.getItem("battleLog"));
+		//Because sometimes the loot counter doesn't update let's do it here again
+		if($("#load-openloot").html().match(/(\d+)/)[1] === "0" && log.CurrentLoot > 0){
+			LiveLog("The Lootbag has been opened without being noticed");
+			log.CurrentLoot = 0;
+			localStorage.setItem("battleLog", JSON.stringify(log));
+		}
 		if(log.CurrentLoot >= inventorySpaces){
 			alert("Not enough inventory space");
 		}
-		var duras = data.match(/remaining durability\: (\d+)/gi);
+		var duras = spellArea.match(/remaining durability\: (\d+)/gi);
 		var output = '| ';
 		for(var i = 0;i<duras.length;i++){
 			var dura = duras[i].match(/(\d+)/)[1];
@@ -281,6 +303,15 @@ function Create_Log_Object(){
 	log.ItemsSold = 0;
 	log.WonWithoutLoot = 0;
 	log.Experience = 0;
+	log.Spell = {};
+	log.Augment = {};
+	log.Enchant = {};
+	var rarities = ["Common", "Superior", "Rare", "Epic", "Legendary"];
+	for(var j=0;i<rarities.length;j++){
+		log.Spell[rarities[j]] = 0;
+		log.Augment[rarities[j]] = 0;
+		log.Enchant[rarities[j]] = 0;
+	}
 	var diffArray = ["Elite", "Hard", "Medium", "Easy"];
 	var keyArray = ["Gold", "Experience", "Won", "Lost", "Loot", "ConsolationLoot", "WonWithoutLoot", "Common", "Superior", "Rare", "Epic", "Legendary"];
 	for(var i=0; i<diffArray.length;i++){
@@ -348,22 +379,38 @@ function SetupLog(){
 function DisenchantSetup(){
 	var rarities = ["Common", "Superior", "Rare", "Epic", "Legendary"];
 	var selects = ["Augment", "Enchant", "Spell"];
-	var table = $(document.createElement("table"));
+	var log = JSON.parse(localStorage.getItem("battleLog"));
+	var table = $(document.createElement("table")).attr({id: 'disenchantingTable'});
 	for(var i=0;i<selects.length;i++){
+		if(log[selects[i]] === undefined){log[selects[i]] = {};} // For older version compatibility
 		var row = $(document.createElement("tr")).attr({'class': 'disenchantingSelect'}).appendTo(table);
 		var info = $(document.createElement("td")).html("Disenchant " + selects[i]  + " up to x rarity");
 		$(info).appendTo(row);
 		var td = $(document.createElement("td"));
-		var select = $(document.createElement("select")).css("font-size", "12px").on("change", function(){
-			localStorage.setItem($(this).attr('id'), $(this).val());
-		});
+
 		for(var j=0;j<rarities.length;j++){
-			var option = $(document.createElement("option")).text(rarities[j]).attr({value:  rarities[j]}).appendTo(select);
-			if(localStorage.getItem("select" + selects[i]) === rarities[j]){
-				option.prop('selected', true);
-			}
+			if(log[selects[i]][rarities[j]] === undefined){log[selects[i]][rarities[j]] = 'false';} // for older version compatibility
+			var checkbox = $(document.createElement("input")).attr({'id': selects[i] + "-" + rarities[j], 'type': 'checkbox'}).on("change", function(){
+				var log = JSON.parse(localStorage.getItem("battleLog"));
+				var temp = $(this).attr('id').split("-");
+				var type = temp[0];
+				var rarity = temp[1];
+				if($(this).prop('checked')){
+					if(debug > 0){
+						LiveLog("D: Set type '" + type + "' with Rarity '" + rarity + "' to 1");
+					}
+					log[type][rarity] = "true";
+				}
+				else{
+					if(debug > 0){
+						LiveLog("D: Set type '" + type + "' with Rarity '" + rarity + "' to 0");
+					}
+					log[type][rarity] = "";
+				}
+				localStorage.setItem("battleLog", JSON.stringify(log));
+			}).prop("checked", log[selects[i]][rarities[j]]).appendTo(td);
+			var span = $(document.createElement("span")).html(rarities[j].match(/(^\w)/)[1]).appendTo(td);
 		}
-		$(select).appendTo(td);
 		$(td).appendTo(row);
 	}
 	var hideRow = $(document.createElement("td")).attr({'colspan': '2'}).css('text-align', 'center').html("Hide Drop-Downs").on("click", function(){
@@ -376,9 +423,11 @@ function DisenchantSetup(){
 			$(this).html("Hide Drop-Downs");
 		}
 	}).appendTo(table);
-	$("td").css('vertical-align', 'middle');
-	$(table).insertAfter($("#slots-remaining"));
+	localStorage.setItem("battleLog", JSON.stringify(log));
+	$(table).insertAfter("#slots-remaining");
+	$("td").css("vertical-align", "middle");
 }
+
 function SelectItemToDisenchant(){
 	var rarities =  ["Common", "Superior", "Rare", "Epic", "Legendary"];
 	var possibleItems = $(".roundResult.areaName").find(".cLink");
@@ -387,9 +436,11 @@ function SelectItemToDisenchant(){
 		var type = item.innerText.match(/(battle|enchant|augment)/i)[1];
 		if(type === "Battle"){type="Spell";} //Remapping
 		var rarity = item.className.match(/card(\w+)/i)[1];
-		if(rarities.indexOf(rarity) < rarities.indexOf(localStorage.getItem("select" + type))){
+		if($("#" + type + "-" + rarity).prop('checked')){ //New change
+			console.log("Item can be DE'd..");
+			console.log($(item));
 			AddEnterShortcut($(item).parents().children().get(0));
-			var span = $(document.createElement("span")).html("<br/>This item will be Disenchanted ").insertAfter($("#selectAugment"));
+			var span = $(document.createElement("span")).html("<br/>This item will be Disenchanted ").insertAfter($("#disenchantingTable"));
 			$(item).clone().appendTo(span);
 			return;
 		}
